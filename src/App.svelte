d460ef2468901a527d4d712b14d76811ad07dede
zinee <zineeworld@gmail.com>
2020-11-03 22:46:52 +0900

    기본 코드

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,36 @@
-<script>
-	import Button from '@/components/Button.svelte';
-	export let name;
-</script>
+<div>
+	<h1>가위바위보!</h1>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-<Button />
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	<h2>{ computer }</h2>
+	<h2>{ print }</h2>
+	<h2>{ result }</h2>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	<button on:click={() => SEND('가위')}>가위</button>
+	<button on:click={() => SEND('바위')}>바위</button>
+	<button on:click={() => SEND('보')}>보</button>
+</div>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style>
 </style>
+
+<script>
+	let num = parseInt(Math.random() * 3)
+	let computer = ''
+	let print = ''
+	let result = ''
+
+	function ACTION() {
+		num = parseInt(Math.random() * 3)
+		if (num == 0) computer = '가위'
+		else if (num == 1) computer = '바위'
+		else if (num == 2) computer = '보'
+	}
+
+	function SEND(my) {
+		clearInterval(timer)
+		print = my
+
+	}
+
+	let timer = setInterval(ACTION, 250)
+</script>
