5cf82801f04705c47170a063408b08795e6eefae
zinee <zineeworld@gmail.com>
2020-11-08 00:11:28 +0900

    #2 짝맞추기

--- src/components/matching-cards/Matching-Cards.svelte
+++ src/components/matching-cards/Matching-Cards.svelte
@@ -1,1 +1,39 @@
-hi
+<div class="container">
+    <div class="card-wrap">
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+        <div class="card-item">{{ n }}</div>
+    </div>
+</div>
+
+<script>
+    let n = 0;
+
+</script>
+
+<style lang="scss">
+    .container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+    .card-wrap {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: auto;
+        width: 400px;
+        height: 200px;
+    }
+    .card-item {
+        display: block;
+        width: calc(25% -5px);
+        height: calc(50% - 10px);
+        background-color:#222;
+    }
+</style>
